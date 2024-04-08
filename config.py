9147026acada556f3e561a5ed9bb60199f066299
Premkumar Ganesan <49362978+premkumar6@users.noreply.github.com>
2024-04-08 13:13:44 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,12 @@
 
 redcap_api_token = '3814EEA9FF90599C2FA94AE7690F1F09'
 dropbox_access_token = 'sl.BlrLD9xUdlLFFlhjbVM3hjwtxCW2pNr81pZzTFgSKpNwjbaQuE7RnPPVCffJ2eRu6xovq0FT2s-MWjtHrI7dQOguDbIHuWaat1DYrEoop-qVh5pnUPFGJw8I-A_IXUFsKn-9i9kYEqHHjdULlOetv0U'
+
+dropbox_app_key = 'm4gafawke1gw6zv'
+dropbox_app_secret = 'q5w1ncfj2r99462'
+my_redirect_uri = 'http://localhost/redirect'
+token_access_type = 'offline'
+
+
+auth_url = f'https://www.dropbox.com/oauth2/authorize?client_id={dropbox_app_key}&token_access_type=offline&response_type=code'
+stored_refresh_token = 'jk71UEw039QAAAAAAAAAASetHx_nmcJ6RGz1I8T0TPit-QKsQsvXOnlT10dJwedU'
